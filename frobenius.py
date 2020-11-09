d0cf1f3b25c1ea4b207113b710c3a4fc660144ed
N.Fujita <naofuji.1220@gmail.com>
2020-11-10 01:05:04 +0900

    横軸を圧縮率に変更

--- frobenius.py
+++ frobenius.py
@@ -7,25 +7,26 @@ from scipy import linalg
 def frob():
     original_np = np.load('study/svd_study/three_eyes.npy').reshape(81,243)     #もとの行列をreshape
     X = original_np.copy()        
-    u, s, v = linalg.svd(X)                 #svd
+    u, s, v = linalg.svd(X)                              #svd
     #norm = np.sqrt(np.sum(X * X))                      #sのフロベニウスノルム
-    norm = np.sum(X * X)                    #sのフロベニウスノルムの二乗
+    norm = np.sum(X * X)                                 #sのフロベニウスノルムの二乗
     x = []
     y = []
     for i in range(0, 82):
-        r = i               #残す特異値の数   
+        r = i                                     #残す特異値の数   
         ur = u[:, :r]
-        sr = np.diag(np.sqrt(s[:r]))       #sの平方根
+        sr = np.diag(np.sqrt(s[:r]))                #sの平方根
         vr = v[:r, :]
         A = ur @ sr
         B = sr @ vr
-        Y = A @ B              #近似した行列
+        Y = A @ B                                   #近似した行列
         #norm1 = np.sqrt(np.sum((X-Y) * (X-Y)))     #フロベニウスノルム
-        norm1 = np.sum((X-Y) * (X-Y))             #フロベニウスノルムの二乗
-        x.append(i)
+        norm1 = np.sum((X-Y) * (X-Y))               #フロベニウスノルムの二乗
+        rate = (A.size+B.size) / X.size            #圧縮率
+        x.append(rate)
         y.append(norm1/ norm)
-    plt.xlabel("rank")
-    plt.ylabel("difference")
+    plt.xlabel("compression ratio")
+    plt.ylabel("relative error of Frobenius norm")
     plt.yscale('log')
     plt.plot(x, y)
     plt.show() 
